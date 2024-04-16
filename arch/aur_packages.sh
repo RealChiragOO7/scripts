2d5972d0d20041386d95daaf4e777bc741bc084d
Chirag Gupta <108172197+RealChiragOO7@users.noreply.github.com>
2024-04-16 13:55:58 +0530

    remove because of errors

--- arch/aur_packages.sh
+++ arch/aur_packages.sh
@@ -1,10 +1,9 @@
 cd ~/Downloads
 
 ide="android-studio arduino-ide-bin visual-studio-code-bin rstudio-desktop-bin"
-motrix="aria2-fast motrix"
 general="anydesk-bin cava google-chrome rate-mirrors sticky-notes ttf-times-new-roman tty-clock yay"
 
-all="$ide $motrix $general"
+all="$ide $general"
 for package in $all; do
     echo "Installing $package"
     git clone https://aur.archlinux.org/"$package".git
